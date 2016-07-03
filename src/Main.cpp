f488895cdbd10abdf362c3321cf53c3e5acd008f
urShadow <videnex@gmail.com>
2016-07-03 21:20:25 +0300

    1.1
    - Fixed bugs.
    - Changed native "match_get_group"

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,15 +1,17 @@
 #include "SDK/amx/amx.h"
 #include "SDK/plugincommon.h"
-#include "Pawn.Regex.inc"
 
 #include <unordered_set>
 #include <vector>
 #include <memory>
 #include <regex>
 
+#include "Pawn.Regex.inc"
+
 using logprintf_t = void(*)(char* format, ...);
 
 logprintf_t logprintf;
+
 extern void *pAMXFunctions;
 
 class Plugin
@@ -17,26 +19,27 @@ class Plugin
 public:
 
 	static constexpr char
-		*Name = "Pawn.Regex",
-		*Version = "1.0",
-		*PublicVarName = "_pawnregex_version";
+		*kName = "Pawn.Regex",
+		*kVersion = "1.1",
+		*kPublicVarName = "_pawnregex_version";
 
 	static bool Load(void)
 	{
-		return logprintf("%s plugin v%s by urShadow loaded", Name, Version), true;
+		return logprintf("%s plugin v%s by urShadow loaded", kName, kVersion), true;
 	}
 
 	static void Unload(void)
 	{
 		_regex_set.clear();
+
 		_match_results_set.clear();
 
-		logprintf("%s plugin v%s by urShadow unloaded", Name, Version);
+		logprintf("%s plugin v%s by urShadow unloaded", kName, kVersion);
 	}
 
 	static void AmxLoad(AMX *amx)
 	{
-		static std::vector<AMX_NATIVE_INFO> native_vec =
+		const static std::vector<AMX_NATIVE_INFO> native_vec =
 		{
 			{ "regex_new", &n_regex_new },
 			{ "regex_delete", &n_regex_delete },
@@ -51,22 +54,22 @@ public:
 		};
 
 		cell addr{}, *phys_addr{};
-		if (!amx_FindPubVar(amx, PublicVarName, &addr) &&
+		if (!amx_FindPubVar(amx, kPublicVarName, &addr) &&
 			!amx_GetAddr(amx, addr, &phys_addr))
 		{
 			if (*phys_addr != PAWNREGEX_INCLUDE_VERSION)
-				return logprintf("[%s] %s: .inc-file version does not equal a plugin version. Recompile a script.", Name, __FUNCTION__);
-		}
+				return logprintf("[%s] %s: .inc-file version does not equal the plugin's version", kName, __FUNCTION__);
 
-		amx_Register(amx, native_vec.data(), native_vec.size());
+			amx_Register(amx, native_vec.data(), native_vec.size());
+		}
 	}
 
 private:
 
-	using regex_ptr = std::shared_ptr<std::regex>;
-	using regex_set = std::unordered_set<regex_ptr>;
-	using match_results_ptr = std::shared_ptr<std::vector<std::string>>;
-	using match_results_set = std::unordered_set<match_results_ptr>;
+	using Regex = std::shared_ptr<std::regex>;
+	using RegexSet = std::unordered_set<Regex>;
+	using MatchResults = std::shared_ptr<std::vector<std::string>>;
+	using MatchResultsSet = std::unordered_set<MatchResults>;
 
 	// native regex:regex_new(const pattern[], E_REGEX_FLAG:flags = REGEX_DEFAULT, E_REGEX_GRAMMAR:grammar = REGEX_ECMASCRIPT);
 	static cell AMX_NATIVE_CALL n_regex_new(AMX *amx, cell *params)
@@ -74,66 +77,31 @@ private:
 		if (!check_params(__FUNCTION__, 3, params))
 			return 0;
 
-		std::unique_ptr<char[]> pattern(get_string(amx, params[1]));
+		const std::unique_ptr<char[]> pattern(get_string(amx, params[1]));
 
-		if (pattern)
+		if (!pattern)
+			return logprintf("[%s] %s: invalid pattern", kName, __FUNCTION__), 0;
+
+		const auto option = get_regex_flag(
+			static_cast<E_REGEX_FLAG>(params[2]),
+			static_cast<E_REGEX_GRAMMAR>(params[3]));
+
+		cell result{};
+
+		try
 		{
-			try
-			{
-				std::regex_constants::syntax_option_type option{};
+			const auto regex = std::make_shared<std::regex>(pattern.get(), option);
 
-				auto flags = static_cast<E_REGEX_FLAG>(params[2]);
-				auto grammar = static_cast<E_REGEX_GRAMMAR>(params[3]);
+			_regex_set.insert(regex);
 
-				switch (grammar)
-				{
-					case REGEX_ECMASCRIPT:
-					default:
-						option = std::regex_constants::ECMAScript;
-						break;
-					case REGEX_BASIC:
-						option = std::regex_constants::basic;
-						break;
-					case REGEX_EXTENDED:
-						option = std::regex_constants::extended;
-						break;
-					case REGEX_AWK:
-						option = std::regex_constants::awk;
-						break;
-					case REGEX_GREP:
-						option = std::regex_constants::grep;
-						break;
-					case REGEX_EGREP:
-						option = std::regex_constants::egrep;
-						break;
-				}
-
-				if (flags & REGEX_ICASE)
-					option |= std::regex_constants::icase;
-				if (flags & REGEX_NOSUBS)
-					option |= std::regex_constants::nosubs;
-				if (flags & REGEX_OPTIMIZE)
-					option |= std::regex_constants::optimize;
-				if (flags & REGEX_COLLATE)
-					option |= std::regex_constants::collate;
-
-				auto regex = std::make_shared<std::regex>(pattern.get(), option);
-
-				_regex_set.insert(regex);
-
-				return reinterpret_cast<cell>(regex.get());
-			}
-			catch (const std::exception &e)
-			{
-				logprintf("[%s] %s: %s", Name, __FUNCTION__, e.what());
-			}
+			result = reinterpret_cast<cell>(regex.get());
 		}
-		else
+		catch (const std::exception &e)
 		{
-			logprintf("[%s] %s: invalid pattern", Name, __FUNCTION__);
+			logprintf("[%s] %s: %s", kName, __FUNCTION__, e.what());
 		}
 
-		return 0;
+		return result;
 	}
 
 	// native regex_delete(&regex:r);
@@ -144,27 +112,19 @@ private:
 
 		cell *cptr{};
 
-		if (!amx_GetAddr(amx, params[1], &cptr))
-		{
-			if (auto regex = get_regex(*cptr))
-			{
-				_regex_set.erase(regex);
+		if (amx_GetAddr(amx, params[1], &cptr) != AMX_ERR_NONE)
+			return logprintf("[%s] %s: invalid param reference", kName, __FUNCTION__), 0;
 
-				*cptr = 0;
+		const auto regex = get_regex(*cptr);
 
-				return 1;
-			}
-			else
-			{
-				logprintf("[%s] %s: invalid regex handle", Name, __FUNCTION__);
-			}
-		}
-		else
-		{
-			logprintf("[%s] %s: invalid param reference", Name, __FUNCTION__);
-		}
+		if (!regex)
+			return logprintf("[%s] %s: invalid regex handle", kName, __FUNCTION__), 0;
 
-		return 0;
+		_regex_set.erase(regex);
+
+		*cptr = 0;
+
+		return 1;
 	}
 
 	// native regex_check(const str[], regex:r, E_MATCH_FLAG:flags = MATCH_DEFAULT);
@@ -173,34 +133,30 @@ private:
 		if (!check_params(__FUNCTION__, 3, params))
 			return 0;
 
-		std::unique_ptr<char[]> str(get_string(amx, params[1]));
+		const std::unique_ptr<char[]> str(get_string(amx, params[1]));
 
-		if (str)
+		if (!str)
+			return logprintf("[%s] %s: invalid str", kName, __FUNCTION__), 0;
+
+		const auto regex = get_regex(params[2]);
+
+		if (!regex)
+			return logprintf("[%s] %s: invalid regex handle", kName, __FUNCTION__), 0;
+
+		const auto flag = get_match_flag(static_cast<E_MATCH_FLAG>(params[3]));
+
+		cell result{};
+
+		try
 		{
-			if (auto regex = get_regex(params[2]))
-			{
-				try
-				{
-					auto flag = get_match_flag(static_cast<E_MATCH_FLAG>(params[3]));
-
-					return static_cast<cell>(std::regex_match(str.get(), *regex, flag));
-				}
-				catch (const std::exception &e)
-				{
-					logprintf("[%s] %s: %s", Name, __FUNCTION__, e.what());
-				}
-			}
-			else
-			{
-				logprintf("[%s] %s: invalid regex handle", Name, __FUNCTION__);
-			}
+			result = static_cast<cell>(std::regex_match(str.get(), *regex, flag));
 		}
-		else
+		catch (const std::exception &e)
 		{
-			logprintf("[%s] %s: invalid str", Name, __FUNCTION__);
+			logprintf("[%s] %s: %s", kName, __FUNCTION__, e.what());
 		}
 
-		return 0;
+		return result;
 	}
 
 	// native regex_match(const str[], regex:r, &match_results:m, E_MATCH_FLAG:flags = MATCH_DEFAULT);
@@ -209,53 +165,47 @@ private:
 		if (!check_params(__FUNCTION__, 4, params))
 			return 0;
 
-		std::unique_ptr<char[]> str(get_string(amx, params[1]));
+		const std::unique_ptr<char[]> str(get_string(amx, params[1]));
 
-		if (str)
+		if (!str)
+			return logprintf("[%s] %s: invalid str", kName, __FUNCTION__), 0;
+
+		const auto regex = get_regex(params[2]);
+
+		if (!regex)
+			return logprintf("[%s] %s: invalid regex handle", kName, __FUNCTION__), 0;
+
+		cell result{};
+
+		try
 		{
-			if (auto regex = get_regex(params[2]))
+			const auto m = std::make_shared<std::vector<std::string>>();
+
+			const auto flag = get_match_flag(static_cast<E_MATCH_FLAG>(params[4]));
+
+			std::cmatch results;
+
+			if (std::regex_match(str.get(), results, *regex, flag))
 			{
-				try
-				{
-					auto m = std::make_shared<std::vector<std::string>>();
+				for (auto &result : results)
+					m->push_back(result.str());
 
-					auto flag = get_match_flag(static_cast<E_MATCH_FLAG>(params[4]));
+				_match_results_set.insert(m);
 
-					std::cmatch results;
+				cell *addr{};
 
-					bool match = std::regex_match(str.get(), results, *regex, flag);
+				if (amx_GetAddr(amx, params[3], &addr) == AMX_ERR_NONE)
+					*addr = reinterpret_cast<cell>(m.get());
 
-					if (match)
-					{
-						for (auto &result : results)
-							m->push_back(result.str());
-
-						_match_results_set.insert(m);
-
-						cell *addr{};
-
-						if (!amx_GetAddr(amx, params[3], &addr))
-							*addr = reinterpret_cast<cell>(m.get());
-
-						return 1;
-					}
-				}
-				catch (const std::exception &e)
-				{
-					logprintf("[%s] %s: %s", Name, __FUNCTION__, e.what());
-				}
-			}
-			else
-			{
-				logprintf("[%s] %s: invalid regex handle", Name, __FUNCTION__);
+				result = 1;
 			}
 		}
-		else
+		catch (const std::exception &e)
 		{
-			logprintf("[%s] %s: invalid str", Name, __FUNCTION__);
+			logprintf("[%s] %s: %s", kName, __FUNCTION__, e.what());
 		}
 
-		return 0;
+		return result;
 	}
 
 	// native regex_search(const str[], regex:r, &match_results:m, &pos, startpos = 0, E_MATCH_FLAG:flags = MATCH_DEFAULT);
@@ -264,58 +214,54 @@ private:
 		if (!check_params(__FUNCTION__, 6, params))
 			return 0;
 
-		std::unique_ptr<char[]> str(get_string(amx, params[1]));
+		const std::unique_ptr<char[]> str(get_string(amx, params[1]));
 
-		if (str)
+		if (!str)
+			return logprintf("[%s] %s: invalid str", kName, __FUNCTION__), 0;
+
+		const std::string s(str.get());
+
+		const auto regex = get_regex(params[2]);
+
+		if (!regex)
+			return logprintf("[%s] %s: invalid regex handle", kName, __FUNCTION__), 0;
+
+		cell result{};
+
+		try
 		{
-			if (auto regex = get_regex(params[2]))
+			const auto m = std::make_shared<std::vector<std::string>>();
+
+			const auto startpos = static_cast<size_t>(params[5]);
+
+			const auto flag = get_match_flag(static_cast<E_MATCH_FLAG>(params[6]));
+
+			std::cmatch results;
+
+			if (std::regex_search(s.substr(startpos).c_str(), results, *regex, flag))
 			{
-				try
-				{
-					auto m = std::make_shared<std::vector<std::string>>();
+				for (auto &result : results)
+					m->push_back(result.str());
 
-					auto startpos = static_cast<size_t>(params[5]);
+				_match_results_set.insert(m);
 
-					auto flag = get_match_flag(static_cast<E_MATCH_FLAG>(params[6]));
+				cell *addr{};
 
-					std::cmatch results;
+				if (amx_GetAddr(amx, params[4], &addr) == AMX_ERR_NONE)
+					*addr = results.position();
 
-					bool match = std::regex_search(&str.get()[startpos], results, *regex, flag);
+				if (amx_GetAddr(amx, params[3], &addr) == AMX_ERR_NONE)
+					*addr = reinterpret_cast<cell>(m.get());
 
-					if (match)
-					{
-						for (auto &result : results)
-							m->push_back(result.str());
-
-						_match_results_set.insert(m);
-
-						cell *addr{};
-
-						if (!amx_GetAddr(amx, params[4], &addr))
-							*addr = results.position();
-
-						if (!amx_GetAddr(amx, params[3], &addr))
-							*addr = reinterpret_cast<cell>(m.get());
-
-						return 1;
-					}
-				}
-				catch (const std::exception &e)
-				{
-					logprintf("[%s] %s: %s", Name, __FUNCTION__, e.what());
-				}
-			}
-			else
-			{
-				logprintf("[%s] %s: invalid regex handle", Name, __FUNCTION__);
+				result = 1;
 			}
 		}
-		else
+		catch (const std::exception &e)
 		{
-			logprintf("[%s] %s: invalid str", Name, __FUNCTION__);
+			logprintf("[%s] %s: %s", kName, __FUNCTION__, e.what());
 		}
 
-		return 0;
+		return result;
 	}
 
 	// native regex_replace(const str[], regex:r, const fmt[], dest[], E_MATCH_FLAG:flags = MATCH_DEFAULT, size = sizeof dest);
@@ -324,71 +270,76 @@ private:
 		if (!check_params(__FUNCTION__, 6, params))
 			return 0;
 
-		std::unique_ptr<char[]>
+		const std::unique_ptr<char[]>
 			str(get_string(amx, params[1])),
 			fmt(get_string(amx, params[3]));
 
-		if (str && fmt)
+		if ((!str) || (!fmt))
+			return logprintf("[%s] %s: invalid str or fmt", kName, __FUNCTION__), 0;
+
+		const auto regex = get_regex(params[2]);
+
+		if (!regex)
+			return logprintf("[%s] %s: invalid regex handle", kName, __FUNCTION__), 0;
+
+		cell result{};
+
+		try
 		{
-			if (auto regex = get_regex(params[2]))
-			{
-				try
-				{
-					auto flag = get_match_flag(static_cast<E_MATCH_FLAG>(params[5]));
+			const auto flag = get_match_flag(static_cast<E_MATCH_FLAG>(params[5]));
 
-					auto size = static_cast<size_t>(params[6]);
+			const auto size = static_cast<size_t>(params[6]);
 
-					auto result = std::regex_replace(str.get(), *regex, fmt.get(), flag);
+			const auto dest = std::regex_replace(str.get(), *regex, fmt.get(), flag);
 
-					set_amxstring(amx, params[4], result.c_str(), size);
+			set_amxstring(amx, params[4], dest.c_str(), size);
 
-					return 1;
-				}
-				catch (const std::exception &e)
-				{
-					logprintf("[%s] %s: %s", Name, __FUNCTION__, e.what());
-				}
-			}
-			else
-			{
-				logprintf("[%s] %s: invalid regex handle", Name, __FUNCTION__);
-			}
+			result = 1;
 		}
-		else
+		catch (const std::exception &e)
 		{
-			logprintf("[%s] %s: invalid str or fmt", Name, __FUNCTION__);
+			logprintf("[%s] %s: %s", kName, __FUNCTION__, e.what());
 		}
 
-		return 0;
+		return result;
 	}
 
-	// native match_get_group(match_results:m, index, dest[], size = sizeof dest);
+	// native match_get_group(match_results:m, index, dest[], &length, size = sizeof dest);
 	static cell AMX_NATIVE_CALL n_match_get_group(AMX *amx, cell *params)
 	{
-		if (!check_params(__FUNCTION__, 4, params))
+		if (!check_params(__FUNCTION__, 5, params))
 			return 0;
 
-		if (auto match_results = get_match_results(params[1]))
-		{
-			auto
-				index = static_cast<size_t>(params[2]),
-				size = static_cast<size_t>(params[4]);
+		const auto match_results = get_match_results(params[1]);
 
-			try
-			{
-				set_amxstring(amx, params[3], match_results->at(index).c_str(), size);
-			}
-			catch (const std::exception &e)
-			{
-				logprintf("[%s] %s: %s", Name, __FUNCTION__, e.what());
-			}
-		}
-		else
+		if (!match_results)
+			return logprintf("[%s] %s: invalid match_results handle", kName, __FUNCTION__), 0;
+
+		const auto
+			index = static_cast<size_t>(params[2]),
+			size = static_cast<size_t>(params[5]);
+
+		cell result{};
+
+		try
 		{
-			logprintf("[%s] %s: invalid match_results handle", Name, __FUNCTION__);
+			const auto str = match_results->at(index);
+
+			set_amxstring(amx, params[3], str.c_str(), size);
+
+			cell *addr{};
+
+			if (amx_GetAddr(amx, params[4], &addr) == AMX_ERR_NONE)
+				*addr = str.length();
+
+			result = 1;
+		}
+		catch (const std::exception &e)
+		{
+			logprintf("[%s] %s: %s", kName, __FUNCTION__, e.what());
 		}
 
-		return 0;
+		return result;
 	}
 
 	// native match_free(&match_results:m);
@@ -399,34 +350,26 @@ private:
 
 		cell *cptr{};
 
-		if (!amx_GetAddr(amx, params[1], &cptr))
-		{
-			if (auto match_results = get_match_results(*cptr))
-			{
-				_match_results_set.erase(match_results);
+		if (amx_GetAddr(amx, params[1], &cptr) != AMX_ERR_NONE)
+			return logprintf("[%s] %s: invalid param reference", kName, __FUNCTION__), 0;
 
-				*cptr = 0;
+		const auto match_results = get_match_results(*cptr);
 
-				return 1;
-			}
-			else
-			{
-				logprintf("[%s] %s: invalid match_results handle", Name, __FUNCTION__);
-			}
-		}
-		else
-		{
-			logprintf("[%s] %s: invalid param reference", Name, __FUNCTION__);
-		}
+		if (!match_results)
+			return logprintf("[%s] %s: invalid match_results handle", kName, __FUNCTION__), 0;
 
-		return 0;
+		_match_results_set.erase(match_results);
+
+		*cptr = 0;
+
+		return 1;
 	}
 
-	static inline regex_ptr get_regex(cell ptr)
+	static inline Regex get_regex(cell ptr)
 	{
-		auto iter = std::find_if(_regex_set.begin(), _regex_set.end(), [ptr](const regex_ptr& r)
+		auto iter = std::find_if(_regex_set.begin(), _regex_set.end(), [ptr](const Regex& p)
 		{
-			return r.get() == reinterpret_cast<void *>(ptr);
+			return p.get() == reinterpret_cast<void *>(ptr);
 		});
 
 		if (iter != _regex_set.end())
@@ -435,17 +378,56 @@ private:
 		return nullptr;
 	}
 
-	static inline match_results_ptr get_match_results(cell ptr)
+	static inline MatchResults get_match_results(cell ptr)
 	{
-		auto iter = std::find_if(_match_results_set.begin(), _match_results_set.end(), [ptr](const match_results_ptr& r)
+		auto iter = std::find_if(_match_results_set.begin(), _match_results_set.end(), [ptr](const MatchResults& p)
 		{
-			return r.get() == reinterpret_cast<void *>(ptr);
+			return p.get() == reinterpret_cast<void *>(ptr);
 		});
 
 		if (iter != _match_results_set.end())
 			return *iter;
 
 		return nullptr;
+	}
+
+	static inline std::regex_constants::syntax_option_type get_regex_flag(E_REGEX_FLAG flags, E_REGEX_GRAMMAR grammar)
+	{
+		std::regex_constants::syntax_option_type flag{};
+
+		switch (grammar)
+		{
+			case REGEX_ECMASCRIPT:
+			default:
+				flag = std::regex_constants::ECMAScript;
+				break;
+			case REGEX_BASIC:
+				flag = std::regex_constants::basic;
+				break;
+			case REGEX_EXTENDED:
+				flag = std::regex_constants::extended;
+				break;
+			case REGEX_AWK:
+				flag = std::regex_constants::awk;
+				break;
+			case REGEX_GREP:
+				flag = std::regex_constants::grep;
+				break;
+			case REGEX_EGREP:
+				flag = std::regex_constants::egrep;
+				break;
+		}
+
+		if (flags & REGEX_ICASE)
+			flag |= std::regex_constants::icase;
+		if (flags & REGEX_NOSUBS)
+			flag |= std::regex_constants::nosubs;
+		if (flags & REGEX_OPTIMIZE)
+			flag |= std::regex_constants::optimize;
+		if (flags & REGEX_COLLATE)
+			flag |= std::regex_constants::collate;
+
+		return flag;
 	}
 
 	static inline std::regex_constants::match_flag_type get_match_flag(E_MATCH_FLAG flags)
@@ -504,14 +486,15 @@ private:
 	static inline bool check_params(const char *native, int count, cell *params)
 	{
 		if (params[0] != (count * sizeof(cell)))
-			return logprintf("[%s] %s: invalid number of parameters. Should be %d", Name, native, count), false;
+			return logprintf("[%s] %s: invalid number of parameters. Should be %d", kName, native, count), false;
 
 		return true;
 	}
 
 	static inline int set_amxstring(AMX *amx, cell amx_addr, const char *source, int max)
 	{
-		cell *dest = reinterpret_cast<cell *>(amx->base + static_cast<int>(reinterpret_cast<AMX_HEADER *>(amx->base)->dat + amx_addr));
+		cell *dest = reinterpret_cast<cell *>(amx->base +
+			static_cast<int>(reinterpret_cast<AMX_HEADER *>(amx->base)->dat + amx_addr));
 
 		cell *start = dest;
 
@@ -523,14 +506,14 @@ private:
 		return dest - start;
 	}
 
-	static regex_set _regex_set;
+	static RegexSet _regex_set;
 
-	static match_results_set _match_results_set;
+	static MatchResultsSet _match_results_set;
 };
 
-Plugin::regex_set Plugin::_regex_set;
+Plugin::RegexSet Plugin::_regex_set;
 
-Plugin::match_results_set Plugin::_match_results_set;
+Plugin::MatchResultsSet Plugin::_match_results_set;
 
 PLUGIN_EXPORT unsigned int PLUGIN_CALL Supports()
 {
